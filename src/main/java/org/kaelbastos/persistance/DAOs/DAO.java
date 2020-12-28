d916dac9431470f7282437a4c4aa0303825bec40
KaelBastos <kaelbastos@hotmail.com>
2020-12-27 23:36:48 -0300

    hashMap persistence of client created to illustrate TDD

--- src/main/java/org/kaelbastos/persistance/DAOs/DAO.java
+++ src/main/java/org/kaelbastos/persistance/DAOs/DAO.java
@@ -1,6 +1,5 @@
 package org.kaelbastos.persistance.DAOs;
 
-<<<<<<< refs/remotes/origin/in_development
 import java.util.List;
 
 public interface DAO <K,T>{
@@ -10,12 +9,4 @@ public interface DAO <K,T>{
     List<T> getAll();
     boolean delete(K k);
 
-=======
-public interface DAO <K,T>{
-    int save(T t);
-    int update(K k, T t);
-    int getOne(K k);
-    int getAll();
-    int delete(K k);
->>>>>>> Deleted controller package and included first test scope
 }
