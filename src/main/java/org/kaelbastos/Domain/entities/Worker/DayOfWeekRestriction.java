d49ba914d513f40719ca442bd36e7c16f0d39e84
KaelBastos <kaelbastos@hotmail.com>
2021-01-07 00:04:51 -0300

    day of week restriction logic change and validator implementation

--- src/main/java/org/kaelbastos/Domain/entities/Worker/DayOfWeekRestriction.java
+++ src/main/java/org/kaelbastos/Domain/entities/Worker/DayOfWeekRestriction.java
@@ -1,21 +1,38 @@
 package org.kaelbastos.Domain.entities.Worker;
 
-import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class DayOfWeekRestriction {
-    private DayOfWeek day;
+    private LocalDateTime start;
+    private LocalDateTime end;
     private String motive;
 
-    public DayOfWeekRestriction(DayOfWeek day, String motive) {
-        this.day = day;
+    public DayOfWeekRestriction(LocalDateTime start, LocalDateTime end, String motive) {
+        this.start = start;
+        this.end = end;
         this.motive = motive;
     }
 
-    public DayOfWeek getDay() {
-        return day;
+    public DayOfWeekRestriction(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
     }
 
-    public void setDay(DayOfWeek day) {
-        this.day = day;
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
     }
 
     public String getMotive() {
@@ -26,4 +43,16 @@ public class DayOfWeekRestriction {
         this.motive = motive;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DayOfWeekRestriction)) return false;
+        DayOfWeekRestriction that = (DayOfWeekRestriction) o;
+        return Objects.equals(getStart(), that.getStart()) && Objects.equals(getEnd(), that.getEnd());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStart(), getEnd());
+    }
 }
---
+++ src/main/java/org/kaelbastos/Domain/entities/Worker/DayOfWeekRestrictionValidator.java
@@ -0,0 +1,30 @@
+package org.kaelbastos.Domain.entities.Worker;
+
+import org.kaelbastos.Domain.entities.utils.Notification;
+import org.kaelbastos.Domain.entities.utils.Validator;
+
+import java.time.LocalDateTime;
+
+public class DayOfWeekRestrictionValidator extends Validator<DayOfWeekRestriction> {
+    @Override
+    public Notification validate(DayOfWeekRestriction dayOfWeekRestriction) {
+        Notification notification = new Notification();
+
+        if(isNotNull(dayOfWeekRestriction)){
+
+            LocalDateTime dayOfWeekRestrictionStart = dayOfWeekRestriction.getStart();
+            LocalDateTime dayOfWeekRestrictionEnd = dayOfWeekRestriction.getEnd();
+
+            if(isNotNull(dayOfWeekRestrictionStart) && isNotNull(dayOfWeekRestrictionEnd)){
+                if(dayOfWeekRestrictionStart.isAfter(dayOfWeekRestrictionEnd))
+                    notification.addError("Start is after end.");
+            } else {
+                notification.addError("Start and/or end are null.");
+            }
+        } else {
+            notification.addError("Day of week restriction is null.");
+        }
+
+        return notification;
+    }
+}
