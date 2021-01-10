fab408bb69b73b82f53ee300185b7589608ed4b5
KaelBastos <kaelbastos@hotmail.com>
2021-01-10 20:11:44 -0300

    Schedule Service implementation

---
+++ src/main/java/org/kaelbastos/Domain/UseCases/ScheduleService.java
@@ -0,0 +1,33 @@
+package org.kaelbastos.Domain.UseCases;
+
+import org.kaelbastos.Domain.Entities.Service.Service;
+import org.kaelbastos.Domain.Entities.Service.ServiceValidator;
+import org.kaelbastos.Domain.Entities.utils.Notification;
+import org.kaelbastos.Persistance.PersistenceFacade;
+
+public class ScheduleService {
+    public boolean schedule(Service service){
+        ServiceValidator serviceValidator = new ServiceValidator();
+        Notification notification = serviceValidator.validate(service);
+
+        PersistenceFacade persistenceFacade = PersistenceFacade.getInstance();
+        if(notification.hasErrors()){
+            throw new IllegalArgumentException(notification.getMessage());
+        } else {
+            if(persistenceFacade.getOneClient(service.getClient().getCpf()).isEmpty())
+                notification.addError("Client does not exists.");
+
+            service.getWorkers().forEach(worker -> {
+                if(persistenceFacade.getOneWorker(worker.getCpf()).isEmpty())
+                    notification.addError("Worker does not exists.");
+                });
+
+            service.getProducts().forEach(product -> {
+                if(persistenceFacade.getOneProduct(product.getId()).isEmpty())
+                    notification.addError("Product does not exists.");
+            });
+        }
+        return persistenceFacade.saveService(service);
+    }
+}
+
