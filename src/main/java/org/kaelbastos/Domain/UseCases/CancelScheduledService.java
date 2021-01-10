c8766c8351b07ab90923810aa0184afba506ca34
KaelBastos <kaelbastos@hotmail.com>
2021-01-10 20:27:19 -0300

    CancelScheduledService implementation

---
+++ src/main/java/org/kaelbastos/Domain/UseCases/CancelScheduledService.java
@@ -0,0 +1,20 @@
+package org.kaelbastos.Domain.UseCases;
+
+import org.kaelbastos.Domain.Entities.Service.Service;
+import org.kaelbastos.Domain.Entities.Service.ServiceStatus;
+import org.kaelbastos.Persistance.PersistenceFacade;
+
+import java.util.Optional;
+
+public class CancelScheduledService {
+
+    public boolean cancel(int serviceId){
+        Optional<Service> optionalService = PersistenceFacade.getInstance()
+                .getOneService(serviceId);
+        if(optionalService.isEmpty())
+            throw new IllegalArgumentException("Service does not exists.");
+        else
+            optionalService.get().setStatus(ServiceStatus.Canceled);
+        return true;
+    }
+}
