a24b65311b9ef7fb53391d79bf838644b6059a8f
KaelBastos <kaelbastos@hotmail.com>
2021-01-10 20:12:09 -0300

    insertClient correction

--- src/test/java/org/kaelbastos/Domain/useCases/InsertClientTest.java
+++ src/test/java/org/kaelbastos/Domain/useCases/InsertClientTest.java
@@ -3,11 +3,11 @@ package org.kaelbastos.Domain.useCases;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.kaelbastos.Domain.Entities.Client.Client;
 import org.kaelbastos.Domain.Entities.Client.ResidenceType;
 import org.kaelbastos.Domain.Entities.utils.Address;
 import org.kaelbastos.Domain.UseCases.InsertClient;
-
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,8 +52,12 @@ class InsertClientTest {
     }
 
     @ParameterizedTest
+    @NullSource
     @MethodSource("provideArgsForMultipleErrorsTest")
     public void validateWithMultipleErrors(Client client) {
+        assertThrows(IllegalArgumentException.class, () -> {
+           insertClient.insert(client);
+        });
     }
 
     private static Stream<Arguments> provideArgsForMultipleErrorsTest() {
