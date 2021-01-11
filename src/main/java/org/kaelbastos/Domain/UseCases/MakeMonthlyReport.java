28e26f98b7845db39ab84804e9cabb6c318b94f4
KaelBastos <kaelbastos@hotmail.com>
2021-01-10 23:37:45 -0300

    MakeMonthlyReport implementation

---
+++ src/main/java/org/kaelbastos/Domain/UseCases/MakeMonthlyReport.java
@@ -0,0 +1,31 @@
+package org.kaelbastos.Domain.UseCases;
+
+import org.kaelbastos.Domain.Entities.Service.Service;
+import org.kaelbastos.Persistance.PersistenceFacade;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class MakeMonthlyReport {
+    public boolean execute(int month){
+        Optional<List<Service>> allServices = PersistenceFacade.getInstance().getAllServices();
+        if(allServices.isEmpty())
+            throw new RuntimeException("No Services saved.");
+        else
+            System.out.println(assembleReport(month));
+        return true;
+    }
+
+    private String assembleReport(int month){
+        Optional<List<Service>> allServices = PersistenceFacade.getInstance().getAllServices();
+        if (allServices.isPresent()) {
+            String services = (String) allServices.get().stream()
+                    .filter(service -> service.getStart().getMonthValue() == month ||
+                                service.getEnd().getMonthValue() == month)
+                    .map(Service::toString)
+                    .collect(Collectors.joining("\n"));
+            return "Services {" + services + "};\n";
+        }
+        return "No services in the selected Month;";
+    }
+}
