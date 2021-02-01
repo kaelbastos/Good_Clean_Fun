c94c11dee4fddf6e4113bc750b7d6a4289b5f912
KaelBastos <kaelbastos@hotmail.com>
2021-02-01 00:51:44 -0300

    Persistence facade merge correction

--- src/main/java/org/kaelbastos/Persistance/PersistenceFacade.java
+++ src/main/java/org/kaelbastos/Persistance/PersistenceFacade.java
@@ -7,11 +7,14 @@ import org.kaelbastos.Domain.Entities.Service.Service;
 import org.kaelbastos.Domain.Entities.Worker.DayOfWeekRestriction;
 import org.kaelbastos.Domain.Entities.Worker.Worker;
 import org.kaelbastos.Domain.Entities.utils.Observation;
+import org.kaelbastos.Domain.Entities.utils.Person;
 import org.kaelbastos.Persistance.HashMap.ClientHashMapDAO;
 import org.kaelbastos.Persistance.HashMap.ProductHashMapDAO;
 import org.kaelbastos.Persistance.HashMap.ServiceHashMapDAO;
 import org.kaelbastos.Persistance.HashMap.WorkerHashMapDAO;
 import org.kaelbastos.Persistance.DAOs.*;
+import org.kaelbastos.Persistance.SQLite.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,10 +23,11 @@ import java.util.Optional;
 public class PersistenceFacade {
     private static PersistenceFacade instance;
 
-    CLientDAO clientDAO = new ClientHashMapDAO();
-    ProductDAO productDAO = new ProductHashMapDAO();
-    ServiceDAO serviceDAO = new ServiceHashMapDAO();
-    WorkerDAO workerDAO = new WorkerHashMapDAO();
+    CLientDAO clientDAO = new ClientSQLiteDAO();
+    ProductDAO productDAO = new ProductSQLiteDAO();
+    ServiceDAO serviceDAO = new ServiceSQLiteDAO();
+    WorkerDAO workerDAO = new WorkerSQLiteDAO();
+    PersonDAO personDAO = new PersonSQLiteDAO();
 
     private PersistenceFacade() {}
 
@@ -53,10 +57,6 @@ public class PersistenceFacade {
         return serviceDAO.getAll();
     }
 
-    public Optional<List<Service>> getServiceByWorker (String workerCpf){
-        return serviceDAO.getServicesByWorker(workerCpf);
-    }
-
     public boolean saveProduct(Product product){
         return productDAO.save(product);
     }
@@ -71,10 +71,6 @@ public class PersistenceFacade {
 
     public Optional<Product> getOneProduct(Integer product){
         return productDAO.getOne(product);
-    }
-
-    public Optional<List<Product>> getAllProducts(){
-        return productDAO.getAll();
     }
 
     public Optional<List<Kit>> getKitsFromProducts(){
@@ -131,5 +127,25 @@ public class PersistenceFacade {
 
     public Optional<ArrayList<Observation>> getObservationsFromClient(String clientId){
         return clientDAO.getObservationsFromClient(clientId);
+    }
+
+    public boolean savePerson(Person person){
+        return personDAO.save(person);
+    }
+
+    public boolean updatePerson(Person person){
+        return personDAO.update(person);
+    }
+
+    public boolean deletePerson(String personCPF){
+        return personDAO.delete(personCPF);
+    }
+
+    public Optional<Person> getOnePerson(String personCPF){
+        return personDAO.getOne(personCPF);
+    }
+
+    public Optional<List<Person>> getAllPerson(){
+        return personDAO.getAll();
     }
 }
